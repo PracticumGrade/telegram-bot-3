7bd860fa0f199d28bc7fa838b61ad0c853eec75e
Pavel Vergeev <vernier_06_curios@icloud.com>
2023-06-29 22:10:01 +0300

    feat: add precode

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater, MessageHandler, Filters
+
+
+kitchen_chat_id = ...
+updater = Updater(token=...)
+menu_url = 'https://practicumgrade.github.io/course-material/bakery-menu.json'
+
+
+def get_menu():
+    response = requests.get(menu_url).json()
+    return response
+
+
+def show_menu(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat.id, get_menu())
+
+
+def process_order(update, context):
+    chat = update.effective_chat
+    if ...:
+        context.bot.send_message(
+            chat_id=kitchen_chat_id,
+            text=...
+        )
+    else:
+        context.bot.send_message(
+            chat_id=kitchen_chat_id,
+            text=...
+        )
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/menu']], resize_keyboard=True)
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Добро пожаловать, {}.'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_message(chat.id, get_menu())
+
+
+updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+updater.dispatcher.add_handler(CommandHandler('menu', show_menu))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, process_order))
+
+updater.start_polling()
+updater.idle()
